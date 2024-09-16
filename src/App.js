aa2e47606edb1e285c57481655909b2622298592
rudtj <kyoungseo03@naver.com>
2024-09-16 17:21:41 +0900

    feat: App 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Outlet,
+} from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import MySurveysPage from "./pages/MySurveysPage";
+import PublicSurveysPage from "./pages/PublicSurveysPage";
+import RewardsPage from "./pages/RewardsPage";
+
+const Layout = () => {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="mySurveys" element={<MySurveysPage />} />
+          <Route path="publicSurveys" element={<PublicSurveysPage />} />
+          <Route path="rewards" element={<RewardsPage />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
