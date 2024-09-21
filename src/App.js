44de560e0ec4135008cc161506d1924cc4d959ed
rudtj <kyoungseo03@naver.com>
2024-09-22 02:03:30 +0900

    feat: settingSurvey 경로 추가

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import HomePage from "./pages/HomePage";
 import MySurveysPage from "./pages/MySurveysPage";
 import PublicSurveysPage from "./pages/PublicSurveysPage";
 import RewardsPage from "./pages/RewardsPage";
+import { EditSurvey } from "./components/features/EditSurvey/index";
+import { SettingSurvey } from "./components/features/SettingSurvey";
 
 const Layout = () => {
   return (
@@ -24,9 +26,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
+          <Route path="home" element={<HomePage />} />
           <Route path="mySurveys" element={<MySurveysPage />} />
           <Route path="publicSurveys" element={<PublicSurveysPage />} />
           <Route path="rewards" element={<RewardsPage />} />
+          <Route path="editSurvey" element={<EditSurvey />} />
+          <Route path="settingSurvey" element={<SettingSurvey />} />
         </Route>
       </Routes>
     </Router>
