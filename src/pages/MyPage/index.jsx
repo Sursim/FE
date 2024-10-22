cc61aa8bdca0d7e09729e3b780de90a2ade0e2ac
rudtj <kyoungseo03@naver.com>
2024-10-22 23:39:26 +0900

    refactor: login 로직 수정

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../routes/AuthProvider";
 import { Header } from "../../components/features/Layout/Header";
-// import { Profile } from "../../components/features/MyPage/Profile";
+import { Profile } from "../../components/features/MyPage/Profile";
 import { LoggedInProfile } from "../../components/features/MyPage/LoggedInProfile";
 import { Footer } from "../../components/features/Layout/Footer";
 
 export default function PublicSurveysPage() {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { isLoggedIn, login, logout } = useAuth();
+  const navigate = useNavigate();
 
-  // const handleLoginSuccess = () => {
-  //   setIsLoggedIn(true);
-  // };
+  const handleLogout = () => {
+    logout();
+    navigate("/home");
+  };
 
   return (
     <Container>
       <Header />
       <ProfileContainer>
-        {/* {isLoggedIn ? (
-          <LoggedInProfile />
-        ) : (
-          <Profile onLoginSuccess={handleLoginSuccess} />
-        )} */}
-        <LoggedInProfile />
+        {isLoggedIn ? <LoggedInProfile /> : <Profile onLoginSuccess={login} />}
       </ProfileContainer>
-      <Button>서비스 탈퇴하기</Button>
+      <Button onClick={handleLogout}>서비스 탈퇴하기</Button>
       <Footer />
     </Container>
   );
@@ -49,4 +48,5 @@ const Button = styled.button`
   align-items: center;
   margin-top: -10px;
   margin-bottom: 50px;
+  cursor: pointer;
 `;
